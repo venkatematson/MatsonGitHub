c70324e6cf3b6d200335214ce1f153958a2e2d34
venkatematson <vnatesan@matson.com>
2016-05-26 14:10:41 -0700

    updated file addnew

--- src/main/java/com/matson/tos/MatsonAddNewFile.java
+++ src/main/java/com/matson/tos/MatsonAddNewFile.java
@@ -4,5 +4,10 @@ package com.matson.tos;
  * Created by VNatesan on 5/26/2016.
  */
 public class MatsonAddNewFile {
+	
+	 public static  void main(String [] args ){
+
+        System.out.println("MatsonGitProcessor");
+    }
 
 }
