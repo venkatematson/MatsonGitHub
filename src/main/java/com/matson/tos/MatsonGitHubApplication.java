1dbf2d4796dc3a664d45b9ded3832026802a89b5
venkatematson <vnatesan@matson.com>
2016-05-26 13:15:52 -0700

    GitHub and IntelliJ integration

---
+++ src/main/java/com/matson/tos/MatsonGitHubApplication.java
@@ -0,0 +1,12 @@
+package com.matson.tos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatsonGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatsonGitHubApplication.class, args);
+	}
+}
