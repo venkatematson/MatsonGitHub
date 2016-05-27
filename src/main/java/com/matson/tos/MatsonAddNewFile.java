4dab69addc7c509ff0f42d2b6cce87f02c1192d3
venkatematson <vnatesan@matson.com>
2016-05-27 10:38:02 -0700

    Fisrt Updated commit to Branch

--- src/main/java/com/matson/tos/MatsonAddNewFile.java
+++ src/main/java/com/matson/tos/MatsonAddNewFile.java
@@ -7,11 +7,8 @@ public class MatsonAddNewFile {
 	
 	 public static  void main(String [] args ){
 
-        System.out.println("MatsonGitProcessor");
-         System.out.println("MatsonGitProcessor2");
-         System.out.println("MatsonGitProcessor");
-         System.out.println("MatsonGitProcessor2");System.out.println("MatsonGitProcessor");
-         System.out.println("MatsonGitProcessor2");
+        System.out.println("Tag Testing with master branch");
+
 
     }
 
